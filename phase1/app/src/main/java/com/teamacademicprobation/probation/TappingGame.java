53d928b4ffe8e71f45b2572c5fae492fa5fd9f96
Zenghao <zenghao.wang@mail.utoronto.ca>
2019-10-21 14:32:12 -0400

    Implemented empty subclasses of Game.java; Each subclass represents a game level: 1. Tapping targets 2. holding the screen for set amount of time 3. Answering trivia questions

---
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/TappingGame.java
@@ -0,0 +1,14 @@
+package com.teamacademicprobation.probation;
+/*The first level of our game; Tapping targets that pop up.*/
+public class TappingGame extends Game {
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
---
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/TriviaGame.java
@@ -0,0 +1,13 @@
+package com.teamacademicprobation.probation;
+/*Timed trivia/quiz questions*/
+public class TriviaGame extends Game {
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
