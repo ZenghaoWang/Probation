b0db5f48844385324739033f20c153afbb8071b8
Zenghao <zenghao.wang@mail.utoronto.ca>
2019-10-21 15:02:16 -0400

    Created static variable for questions for the trivia game

--- phase1/app/src/main/java/com/teamacademicprobation/probation/TriviaGame.java
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/TriviaGame.java
@@ -1,6 +1,15 @@
 package com.teamacademicprobation.probation;
+
+import java.util.HashMap;
+import java.util.ArrayList;
 /*Timed trivia/quiz questions*/
 public class TriviaGame extends Game {
+    // Keys are questions, values are a list of multiple choice answers
+    private static final HashMap<String, ArrayList<String>> QUESTIONS_TO_ANSWERS = new HashMap<>();
+    static{
+//        QUESTIONS_TO_ANSWERS.put()
+    }
+
     @Override
     public void play() {
 
