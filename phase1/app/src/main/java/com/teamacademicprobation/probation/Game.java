3a3e316a680e0dacc7489ac9739cca5dc258a66c
Zenghao <zenghao.wang@mail.utoronto.ca>
2019-10-21 14:17:56 -0400

    Commented level ideas to subclass Game.java with

--- phase1/app/src/main/java/com/teamacademicprobation/probation/Game.java
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/Game.java
@@ -1,6 +1,11 @@
 package com.teamacademicprobation.probation;
 
 public abstract class Game {
+    /* Game ideas to subclass
+    * Tapping things
+    * holding screen for amount of time??
+    * Timed Trivia
+    * */
     public abstract void play();
     public abstract void render();
 }
