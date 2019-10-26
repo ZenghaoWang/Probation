04b764799fdf2eae925f0e63a55dda136a32fa6a
Samuel <samuel.vedrik@mail.utoronto.ca>
2019-10-25 21:58:25 -0400

    "Changed HoldingGame to TimingGame"

--- phase1/app/src/main/java/com/teamacademicprobation/probation/HoldingGame.java
+++
@@ -1,9 +0,0 @@
-package com.teamacademicprobation.probation;
-/*Holding the screen for as close to the specified amount of time as possible.*/
-public class HoldingGame extends Game {
-  @Override
-  public void play() {}
-
-  @Override
-  public void render() {}
-}
---
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/TImingGame.java
@@ -0,0 +1,14 @@
+package com.teamacademicprobation.probation;
+
+
+/**
+ * A game where there is a bar with a target box and a moving line, and the player attempts to time
+ * their taps so the line ends up inside the target box. 
+ */
+public class TImingGame extends Game {
+  @Override
+  public void play() {}
+
+  @Override
+  public void render() {}
+}
