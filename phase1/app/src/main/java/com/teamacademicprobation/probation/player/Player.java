d59ddae24140ebc7da4b1f724ef6bffb2e7764ff
Jennifer <jennifer.ruan@mail.utoronto.ca>
2019-11-01 01:11:46 -0400

    "Made it so PlayerStats are initiated with an empty currGame and bestGame instead of null."

--- phase1/app/src/main/java/com/teamacademicprobation/probation/player/Player.java
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/player/Player.java
@@ -4,161 +4,149 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-/**
- * A player for the game.
- */
+/** A player for the game. */
 public class Player {
 
-    /**
-     * The statistics of this player.
-     */
-    private PlayerStats playerStats;
+  /** The statistics of this player. */
+  private PlayerStats playerStats;
 
-    /**
-     * The preferences of this player.
-     */
+  /** The preferences of this player. */
 
-    // TODO: Can we just use a map for playerPreferences? Hmm..
-    private PlayerPreferences playerPreferences;
+  // TODO: Can we just use a map for playerPreferences? Hmm..
+  private PlayerPreferences playerPreferences;
 
-    /**
-     * The username of this player.
-     */
-    private String username;
+  /** The username of this player. */
+  private String username;
 
-    /**
-     * The password of this player.
-     */
-    private String password;
+  /** The password of this player. */
+  private String password;
 
-    /**
-     * The random playerID of this player
-     */
-    private String playerID;
+  /** The random playerID of this player */
+  private String playerID;
 
-    private static final int playerIDlen = 5;
+  private static final int playerIDlen = 5;
 
-    public Player() {
-        this("defaultUser", "defaultPass");
+  public Player() {
+    this("defaultUser", "defaultPass");
+  }
 
+  public Player(String username, String password) {
+    this.username = username;
+    this.password = password;
+    this.playerStats = new PlayerStats();
+    this.playerPreferences = new PlayerPreferences();
+    this.playerID = generateRandomID();
+  }
+
+  // ==== SETTER/GETTER METHODS ====
+  public String getUsername() {
+    return this.username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setPlayerID(String playerID) {
+    this.playerID = playerID;
+  }
+
+  // ==== END OF SETTER/GETTER METHODS ====
+
+  private String generateRandomID() {
+    String lowercase = "abcdefghijklmnopqrstuvwxyz";
+    String uppercase = lowercase.toUpperCase();
+    String data = uppercase + lowercase;
+    Random random = new Random();
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < playerIDlen; i++) {
+      result.append(data.charAt(random.nextInt(data.length())));
     }
+    return result.toString();
+  }
 
-    public Player(String username, String password) {
-        this.username = username;
-        this.password = password;
-        this.playerStats = new PlayerStats();
-        this.playerPreferences = new PlayerPreferences();
-        this.playerID = generateRandomID();
+  public Map<String, Object> getData() {
+    Map<String, Object> result = new HashMap<>();
+
+    // Add username and password
+    result.put("Username", this.username);
+    result.put("Password", this.password);
+
+    Map<String, String> preferences = new HashMap<>();
+    for (String preferenceKey : this.playerPreferences.getPreferences().keySet()) {
+      preferences.put(preferenceKey, this.playerPreferences.getPreference(preferenceKey));
     }
+    result.put("Preferences", preferences);
 
-    // ==== SETTER/GETTER METHODS ====
-    public String getUsername() {
-        return this.username;
+    Map<String, Map<String, Integer>> currStats = new HashMap<>();
+    currStats.put(
+        this.playerStats.getCurrLevel(), this.playerStats.getCurr()); // this is null so fix it
+    result.put("Current Session", currStats);
+
+    Map<String, Map<String, Integer>> bestStats = new HashMap<>();
+    for (String gameKey : this.playerStats.getBest().keySet()) {
+      if (!gameKey.equals("")) {
+        bestStats.put(gameKey, this.playerStats.getBest(gameKey));
+      }
     }
+    result.put("Best Session", bestStats);
 
-    public String getPassword() {
-        return this.password;
+    return result;
+  }
+
+  /**
+   * Returns the playerID of this player.
+   *
+   * @return PlayerID.
+   */
+  public String getPlayerID() {
+    return this.playerID;
+  }
+
+  /**
+   * Updates PlayerPreferences.
+   *
+   * @param playerPreferences A map with the following format: {"Preference1": String}
+   */
+  public void updatePreferences(Map<String, String> playerPreferences) {
+    for (String preferenceKey : playerPreferences.keySet()) {
+      this.playerPreferences.updatePreference(preferenceKey, playerPreferences.get(preferenceKey));
     }
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  /**
+   * Lets you update the player's current game session with a new game.
+   *
+   * @param currGameID the new game's ID
+   */
+  public void updateCurrGame(String currGameID) {
+    this.playerStats.newGame(currGameID);
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  /**
+   * Updates the PlayerStats for the current game.
+   *
+   * @param statID the statistic to be updated
+   * @param value the new value of the statistic
+   */
+  public void updateCurrStats(String statID, int value) {
+    this.playerStats.updateCurrGame(statID, value);
+  }
 
-    public void setPlayerID(String playerID) {
-        this.playerID = playerID;
-    }
-
-    // ==== END OF SETTER/GETTER METHODS ====
-
-    private String generateRandomID() {
-        String lowercase = "abcdefghijklmnopqrstuvwxyz";
-        String uppercase = lowercase.toUpperCase();
-        String data = uppercase + lowercase;
-        Random random = new Random();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < playerIDlen; i++) {
-            result.append(data.charAt(random.nextInt(data.length())));
-
-        }
-        return result.toString();
-    }
-
-    public Map<String, Object> getData() {
-        Map<String, Object> result = new HashMap<>();
-
-        // Add username and password
-        result.put("Username", this.username);
-        result.put("Password", this.password);
-
-        Map<String, String> preferences = new HashMap<>();
-        for (String preferenceKey : this.playerPreferences.getPreferences().keySet()) {
-            preferences.put(preferenceKey, this.playerPreferences.getPreference(preferenceKey));
-        }
-        result.put("Preferences", preferences);
-
-        Map<String, Map<String, Integer>> currStats = new HashMap<>();
-        currStats.put(this.playerStats.getCurrLevel(), this.playerStats.getCurr()); // this is null so fix it
-        result.put("Current Session", currStats);
-
-        Map<String, Map<String, Integer>> bestStats = new HashMap<>();
-        for (String gameKey : this.playerStats.getBest().keySet()) {
-            bestStats.put(gameKey, this.playerStats.getBest(gameKey));
-        }
-        result.put("Best Session", bestStats);
-
-        return result;
-    }
-
-    /**
-     * Returns the playerID of this player.
-     *
-     * @return PlayerID.
-     */
-    public String getPlayerID() {
-        return this.playerID;
-    }
-
-    /**
-     * Updates PlayerPreferences.
-     *
-     * @param playerPreferences A map with the following format: {"Preference1": String}
-     */
-    public void updatePreferences(Map<String, String> playerPreferences) {
-        for (String preferenceKey : playerPreferences.keySet()) {
-            this.playerPreferences.updatePreference(preferenceKey, playerPreferences.get(preferenceKey));
-        }
-    }
-
-    /**
-     * Lets you update the player's current game session with a new game.
-     *
-     * @param currGameID the new game's ID
-     */
-    public void updateCurrGame(String currGameID) {
-        this.playerStats.newGame(currGameID);
-    }
-
-    /**
-     * Updates the PlayerStats for the current game.
-     *
-     * @param statID the statistic to be updated
-     * @param value  the new value of the statistic
-     */
-    public void updateCurrStats(String statID, int value) {
-        this.playerStats.updateCurrGame(statID, value);
-    }
-
-    /**
-     * Updates the PlayerStats for the current game.
-     *
-     * @param gameStatsMap A map with the following format: {"Stat1": int}
-     */
-    public void updateCurrStats(Map<String, Integer> gameStatsMap) {
-        this.playerStats.updateCurrGame(gameStatsMap);
-    }
-
+  /**
+   * Updates the PlayerStats for the current game.
+   *
+   * @param gameStatsMap A map with the following format: {"Stat1": int}
+   */
+  public void updateCurrStats(Map<String, Integer> gameStatsMap) {
+    this.playerStats.updateCurrGame(gameStatsMap);
+  }
 }
--- phase1/app/src/main/java/com/teamacademicprobation/probation/player/PlayerStats.java
+++ phase1/app/src/main/java/com/teamacademicprobation/probation/player/PlayerStats.java
@@ -1,140 +1,138 @@
 package com.teamacademicprobation.probation.player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 /**
- * A player's game statistics across all their played games. Keeps a record of this player's
- * current game, current game's stats, their best game stats, and all the games they've played.
+ * A player's game statistics across all their played games. Keeps a record of this player's current
+ * game, current game's stats, their best game stats, and all the games they've played.
  */
 public class PlayerStats {
 
-    private PlayerGameStats currGame;
+  private PlayerGameStats currGame;
 
-    // A map of each type of game along with the player's best scores within that game.
-    private Map<String, PlayerGameStats> bestGame;
+  // A map of each type of game along with the player's best scores within that game.
+  private Map<String, PlayerGameStats> bestGame = new HashMap<>();
 
-    // A list of all the player's completed games.
-    private final ArrayList<PlayerGameStats> completedGames; //how to retrieve specific game stats?
+  // A list of all the player's completed games.
+  private final ArrayList<PlayerGameStats> completedGames; // how to retrieve specific game stats?
 
-    /**
-     * Instantiates a PlayerStats object with an empty list of completed games.
-     */
-    PlayerStats() {
-        currGame = null;
-        bestGame = null;
-        completedGames = new ArrayList<>();
+  /** Instantiates a PlayerStats object with an empty list of completed games. */
+  PlayerStats() {
+    currGame = new PlayerGameStats("");
+    bestGame.put("", currGame);
+    completedGames = new ArrayList<>();
+  }
+
+  /**
+   * Instantiates a new PlayerGameStats object with the given gameID (ie. when you start a new game)
+   *
+   * @param gameID which game the player is currently on.
+   */
+  public void newGame(String gameID) {
+    currGame = new PlayerGameStats(gameID);
+  }
+
+  /** Occurs at the end of the current game session. */
+  public void endCurrGame() {
+    completedGames.add(currGame); // adds this game to the list of this player's completed games
+
+    updateBestGame(currGame); // updates the player's best game statistics with this game in mind
+
+    currGame
+        .setComplete(); // sets the current game to complete (meaning it's not the current level)
+    currGame =
+        new PlayerGameStats(
+            ""); // sets the current game to null (meaning there is currently no level being played)
+  }
+
+  /**
+   * Updates a single statistic within the current game.
+   *
+   * @param statID the statistic to be updated.
+   * @param value the value we want associated with that statistic.
+   */
+  public void updateCurrGame(String statID, int value) {
+    currGame.update(statID, value);
+  }
+
+  /**
+   * Updates a map of statistics within the current game.
+   *
+   * @param newStats a map of statistics plus the values we want associated with them
+   */
+  public void updateCurrGame(Map<String, Integer> newStats) {
+    newStats.forEach((k, v) -> currGame.update(k, v));
+  }
+
+  /**
+   * Updates the statistics of the player's best game if the new statistics are better
+   *
+   * @param otherGameStats the statistics of the game to be compared with the best game statistics
+   */
+  private void updateBestGame(PlayerGameStats otherGameStats) {
+    String gameID = otherGameStats.getGameID();
+
+    if (bestGame.containsKey(gameID)) {
+      if (Objects.requireNonNull(bestGame.get(gameID)).compareTo(otherGameStats) < 0) {
+        bestGame.put(gameID, otherGameStats);
+      }
+    } else {
+      bestGame.put(gameID, otherGameStats);
     }
+  }
 
-    /**
-     * Instantiates a new PlayerGameStats object with the given gameID (ie. when you start a new
-     * game)
-     *
-     * @param gameID which game the player is currently on.
-     */
-    public void newGame(String gameID) {
-        currGame = new PlayerGameStats(gameID);
-    }
+  public String getCurrLevel() {
+    return currGame.getGameID();
+  }
 
-    /**
-     * Occurs at the end of the current game session.
-     */
-    public void endCurrGame() {
-        completedGames.add(currGame); //adds this game to the list of this player's completed games
+  /**
+   * Returns a map of all the statistics of the current game along with their values.
+   *
+   * @return the map
+   */
+  public Map<String, Integer> getCurr() {
+    return currGame.getAllStats();
+  }
 
-        updateBestGame(currGame); // updates the player's best game statistics with this game in mind
+  /**
+   * Returns the current game's current score for a certain statistic.
+   *
+   * @param statID the statistic to be returned.
+   * @return the statistic's value.
+   */
+  public int getCurr(String statID) {
+    return currGame.getStat(statID);
+  }
 
-        currGame.setComplete(); // sets the current game to complete (meaning it's not the current level)
-        currGame = null; // sets the current game to null (meaning there is currently no level being played)
-    }
+  /**
+   * Returns a map of the player's highest scores for each game.
+   *
+   * @return a map of all the statistics of a game along with their associated values.
+   */
+  public Map<String, PlayerGameStats> getBest() {
+    return Objects.requireNonNull(bestGame);
+  }
 
-    /**
-     * Updates a single statistic within the current game.
-     *
-     * @param statID the statistic to be updated.
-     * @param value  the value we want associated with that statistic.
-     */
-    public void updateCurrGame(String statID, int value) {
-        currGame.update(statID, value);
-    }
+  /**
+   * Returns a map of the player's highest scores for each game.
+   *
+   * @return a map of all the statistics of a game along with their associated values.
+   */
+  public Map<String, Integer> getBest(String gameID) {
+    return Objects.requireNonNull(bestGame.get(gameID)).getAllStats();
+  }
 
-    /**
-     * Updates a map of statistics within the current game.
-     *
-     * @param newStats a map of statistics plus the values we want associated with them
-     */
-    public void updateCurrGame(Map<String, Integer> newStats) {
-        newStats.forEach((k, v) -> currGame.update(k, v));
-    }
-
-    /**
-     * Updates the statistics of the player's best game if the new statistics are better
-     *
-     * @param otherGameStats the statistics of the game to be compared with the best game statistics
-     */
-    private void updateBestGame(PlayerGameStats otherGameStats) {
-        String gameID = otherGameStats.getGameID();
-
-        if (bestGame.containsKey(gameID)) {
-            if (Objects.requireNonNull(bestGame.get(gameID)).compareTo(otherGameStats) < 0) {
-                bestGame.put(gameID, otherGameStats);
-            }
-        } else {
-            bestGame.put(gameID, otherGameStats);
-        }
-    }
-
-    public String getCurrLevel() {
-        return currGame.getGameID();
-    }
-
-    /**
-     * Returns a map of all the statistics of the current game along with their values.
-     *
-     * @return the map
-     */
-    public Map<String, Integer> getCurr() {
-        return currGame.getAllStats();
-    }
-
-    /**
-     * Returns the current game's current score for a certain statistic.
-     *
-     * @param statID the statistic to be returned.
-     * @return the statistic's value.
-     */
-    public int getCurr(String statID) {
-        return currGame.getStat(statID);
-    }
-
-    /**
-     * Returns a map of the player's highest scores for each game.
-     *
-     * @return a map of all the statistics of a game along with their associated values.
-     */
-    public Map<String, PlayerGameStats> getBest() {
-        return Objects.requireNonNull(bestGame);
-    }
-
-    /**
-     * Returns a map of the player's highest scores for each game.
-     *
-     * @return a map of all the statistics of a game along with their associated values.
-     */
-    public Map<String, Integer> getBest(String gameID) {
-        return Objects.requireNonNull(bestGame.get(gameID)).getAllStats();
-    }
-
-    /**
-     * Returns the "highest score" for a given game type's given statistic.
-     *
-     * @param gameID the game whose statistics we want.
-     * @param statID the statistic we want.
-     * @return a map of the game's statistics.
-     */
-    public int getBest(String gameID, String statID) {
-        return Objects.requireNonNull(bestGame.get(gameID)).getStat(statID);
-    }
-
+  /**
+   * Returns the "highest score" for a given game type's given statistic.
+   *
+   * @param gameID the game whose statistics we want.
+   * @param statID the statistic we want.
+   * @return a map of the game's statistics.
+   */
+  public int getBest(String gameID, String statID) {
+    return Objects.requireNonNull(bestGame.get(gameID)).getStat(statID);
+  }
 }
